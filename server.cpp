a6c68ff1971d619dc12242bf8baf10904de60b2d
BigBossAnwer <salman.anwer.tr@gmail.com>
2017-04-21 19:25:40 -0500

    Fixed logic of argc validation

--- server.cpp
+++ server.cpp
@@ -2,17 +2,32 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc > 4 || argc < 2) {
-		error("Please provide up to 3 ports to monitor.");
-		return 1;
-	}
-	if (fork() == 0)
-		startServer(9999, echoResult_tcp, log);
-	else if (fork() == 0)
-		startServer(atoi(argv[1]), echoResult_tcp, echoResult_udp);
-	else if (fork() == 0)
-		startServer(atoi(argv[2]), echoResult_tcp, echoResult_udp);
-	else
-		startServer(atoi(argv[3]), echoResult_tcp, echoResult_udp);
+	switch (argc)
+		{
+			case 2:
+				if (fork() == 0) startServer(9999, echoResult_tcp, log);
+				if (fork() == 0)
+					startServer(atoi(argv[1]), echoResult_tcp, echoResult_udp);
+				break;
+			case 3:
+				if (fork() == 0) startServer(9999, echoResult_tcp, log);
+				if (fork() == 0)
+					startServer(atoi(argv[1]), echoResult_tcp, echoResult_udp);
+				if (fork() == 0)
+					startServer(atoi(argv[2]), echoResult_tcp, echoResult_udp);
+				break;
+			case 4:
+				if (fork() == 0) startServer(9999, echoResult_tcp, log);
+				if (fork() == 0)
+					startServer(atoi(argv[1]), echoResult_tcp, echoResult_udp);
+				if (fork() == 0)
+					startServer(atoi(argv[2]), echoResult_tcp, echoResult_udp);
+				if (fork() == 0)
+					startServer(atoi(argv[3]), echoResult_tcp, echoResult_udp);
+				break;
+			default:
+				error("Please provide up to 3 ports to monitor like so: ./server port1 port2 port3 (eg. ./server 1000, 1001, 1002)");
+				return 1;
+		}
     return 0; 
 }
