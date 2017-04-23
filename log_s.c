82a460b32e785e1081ffb73511c94239eaa9a327
BigBossAnwer <salman.anwer.tr@gmail.com>
2017-04-23 18:00:09 -0500

    Formats + Header elaboration

--- log_s.c
+++ log_s.c
@@ -2,7 +2,8 @@
 // authors: DY TD SW SA AK JA
 // date: 04/20/2017
 // purpose: CS 3376 - 502
-// description: A server that logs messages sent
+// description: Log server that communicates with echo_s to log the messages echo_s receives
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,7 +17,8 @@
 #include <time.h>
 
 //TD: starts the log server (UDP) upon being called on in the server
-int startLogServer(int portno){
+int startLogServer(int portno)
+{
 	int sockfd;
 	struct sockaddr_in serv_addr;
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -37,36 +39,41 @@ int startLogServer(int portno){
 //TD: handles zombies
 void *SigCatcher(int n)
 {
-  wait3(NULL,WNOHANG,NULL);
+	wait3(NULL,WNOHANG,NULL);
 }
+
 //SW: loops listens on "portno" and send message to function "writetofile"
 void udp_loop(int udp_sockfd)
 {
-      int n;
-      struct sockaddr_in from;
-      socklen_t fromlen;
-      char buf[1024];
-      fromlen = sizeof(struct sockaddr_in);
-      while(1)
-      {
+	int n;
+	struct sockaddr_in from;
+	socklen_t fromlen;
+	char buf[1024];
+	fromlen = sizeof(struct sockaddr_in);
+	while(1)
+	{
 	   n = recvfrom(udp_sockfd,buf,1024,0,(struct sockaddr *)&from,&fromlen);
 	   if(n<0) error("ERROR recvfrom");
-	   write(1,buf,n);
+			write(1,buf,n);
 	   //TD: writes to the echo.log file and passes the message
 	   writetofile(buf);
 	   if(n<0) error("ERROR sendto");
 		     signal(SIGCHLD,SigCatcher);
-      }
+	}
 }
+
 //SW: function to write to file
 //function accepts "buf", the message and ip address, and write to file "echo.log"
 void writetofile(char buf[1024])
 {
 	FILE *fw;
-	fw = fopen("echo.log", "a"); //SW: makes echo.log it if does not exist, append to it if it does
+	//SW: makes echo.log it if does not exist, append to it if it does
+	fw = fopen("echo.log", "a"); 
 	time_t ti = time(NULL);
-	struct tm t = *localtime(&ti); //SW: get the time 
+	//SW: get the time 
+	struct tm t = *localtime(&ti); 
 	fprintf(fw,"%d-%d-%d %d:%d:%d\t%s\n", t.tm_year+1900, t.tm_mon+1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec, buf); //SW: write date, message, and ipaddress to file
-	fclose(fw); //SW: save file
+	//SW: save file
+	fclose(fw); 
 }
 
