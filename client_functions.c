217bc18244aa1a5d7bb334fa47ab96ce786c750c
BigBossAnwer <salman.anwer.tr@gmail.com>
2017-04-23 17:53:25 -0500

    Smol formatting + comments

--- client_functions.c
+++ client_functions.c
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+//SA: Issued when a system call fails. Displays error message and aborts
 void error(const char *msg)
 {
     perror(msg);
@@ -20,7 +21,8 @@ void error(const char *msg)
 }
 
 //TD: connects the client for TCP
-int clientConnect_tcp(char* host, int portno, void callback(int)) {
+int clientConnect_tcp(char* host, int portno, void callback(int)) 
+{
 	int sockfd;
     struct sockaddr_in serv_addr;
     struct hostent *server;
@@ -29,15 +31,14 @@ int clientConnect_tcp(char* host, int portno, void callback(int)) {
     if (sockfd < 0) 
         error("ERROR opening socket");
     server = gethostbyname(host);
-    if (server == NULL) {
+    if (server == NULL) 
+    {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr_list[0], 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char *)server->h_addr_list[0], (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
 	if (connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0)
 		error("connect");
@@ -45,25 +46,25 @@ int clientConnect_tcp(char* host, int portno, void callback(int)) {
 	return 0;
 }
 
-int clientConnect_udp(char* host, int portno, void callback(int, struct sockaddr_in)) {
-	int sockfd;
+//SA: Handles UDP client connections. Connects to the port# passed in
+int clientConnect_udp(char* host, int portno, void callback(int, struct sockaddr_in))
+{
+    int sockfd;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
+    if (sockfd < 0) error("ERROR opening socket");
     server = gethostbyname(host);
-    if (server == NULL) {
+    if (server == NULL) 
+    {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr_list[0], 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char *)server->h_addr_list[0], (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
-	callback(sockfd, serv_addr);
-	return 0;
+    callback(sockfd, serv_addr);
+    return 0;
 }
